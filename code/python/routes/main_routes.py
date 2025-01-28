aee9260dc432266c56f3d7dd3bd6f1ffb1b04f73
Rene <Rene.Mlodoch@gmail.com>
2025-01-28 11:05:08 +0100

    Create main_routes.py

---
+++ code/python/routes/main_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+main_routes = Blueprint('main_routes', __name__)
+
+@main_routes.route('/api/status')
+def status():
+    return render_template('status.html')
