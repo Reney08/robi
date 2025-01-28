1c8db0f4beea1e3b8a085143428ec8e9a272d5a1
Rene <Rene.Mlodoch@gmail.com>
2025-01-28 13:26:20 +0100

    Update servo_routes.py

--- code/python/routes/servo_routes.py
+++ code/python/routes/servo_routes.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 servo_routes = Blueprint('servo_routes', __name__)
 
-@servo_routes.route('/api/servo/status')
+@servo_routes.route('/status')
 def servo_status():
     return render_template('servo_status.html')
