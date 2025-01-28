007a21d53ab63a42f2179333a7f3e0536d7a07c9
Rene <Rene.Mlodoch@gmail.com>
2025-01-28 14:43:04 +0100

    Update main_routes.py

--- code/python/routes/main_routes.py
+++ code/python/routes/main_routes.py
@@ -2,10 +2,6 @@ from flask import Blueprint, render_template
 
 main_routes = Blueprint('main_routes', __name__)
 
-@main_routes.route('/')
-def home():
-    return render_template('status.html')
-    
 @main_routes.route('/status')
 def status():
     return render_template('status.html')
