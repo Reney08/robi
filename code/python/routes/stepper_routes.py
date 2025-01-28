e579666af750bded21d8ee8e22485b2a4676488e
Rene <Rene.Mlodoch@gmail.com>
2025-01-28 14:43:31 +0100

    Update stepper_routes.py

--- code/python/routes/stepper_routes.py
+++ code/python/routes/stepper_routes.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 stepper_routes = Blueprint('stepper_routes', __name__)
 
-@stepper_routes.route('r/status')
+@stepper_routes.route('/status')
 def stepper_status():
     return render_template('stepper_status.html')
