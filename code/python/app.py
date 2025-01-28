732e1c014b12146f6eee6b2fb753a8cc611f6d55
Rene <Rene.Mlodoch@gmail.com>
2025-01-28 14:42:39 +0100

    Update app.py

--- code/python/app.py
+++ code/python/app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 class FlaskApp:
     def __init__(self):
-        self.app = Flask(__name__, template_folder='../html/template', static_folder='../html/static')
+        self.app = Flask(__name__, template_folder='../html/templates', static_folder='../html/static')
         self.register_routes()
 
     def register_routes(self):
         from routes import register_blueprints
         register_blueprints(self.app)
+
+        @self.app.route('/')
+        def home():
+            return redirect(url_for('main_routes.status'))
 
     def run(self):
         self.app.run(host='127.0.0.1', port=5000)
