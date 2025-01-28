d675e14a690d449bb81aebfe0ee33dfa7a89687b
Rene <Rene.Mlodoch@gmail.com>
2025-01-28 11:04:31 +0100

    Create app.py

---
+++ code/python/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+class FlaskApp:
+    def __init__(self):
+        self.app = Flask(__name__, template_folder='../html/template', static_folder='../html/static')
+        self.register_routes()
+
+    def register_routes(self):
+        from routes import register_blueprints
+        register_blueprints(self.app)
+
+    def run(self):
+        self.app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    flask_app = FlaskApp()
+    flask_app.run()
