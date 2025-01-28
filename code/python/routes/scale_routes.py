02da28fc356750171e365b100e46dd4d89d8437b
Rene <Rene.Mlodoch@gmail.com>
2025-01-28 13:26:29 +0100

    Update scale_routes.py

--- code/python/routes/scale_routes.py
+++ code/python/routes/scale_routes.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 scale_routes = Blueprint('scale_routes', __name__)
 
-@scale_routes.route('/api/scale/status')
+@scale_routes.route('/status')
 def scale_status():
     return render_template('scale_status.html')
