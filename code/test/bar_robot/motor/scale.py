5edd8af562b2fa6e9e5da64e2b400c12b7e0166a
Rene <Rene.Mlodoch@gmail.com>
2025-02-04 16:06:49 +0100

    Update scale.py

--- code/test/bar_robot/motor/scale.py
+++ code/test/bar_robot/motor/scale.py
@@ -6,3 +6,7 @@ class Scale:
         return {
             'weight': 0  # Placeholder value
         }
+        
+    def shutdown(self):
+        # Add any necessary cleanup code for the scale here
+        print("Shutting down scale")
