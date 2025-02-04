e8cd093fc06283ca2f82ab2518e75b4e4c691909
Rene <Rene.Mlodoch@gmail.com>
2025-02-04 10:41:21 +0100

    Update app.py

--- code/test/bar_robot/app.py
+++ code/test/bar_robot/app.py
@@ -24,6 +24,7 @@ def selected_cocktail(selected_cocktail):
 @app.route('/start_mixing', methods=['POST'])
 def start_mixing():
     cocktail = request.form['cocktail']
+    sequence = stepper.load_sequence(cocktail)
     if sequence:
         stepper.execute_sequence(sequence)
     return redirect(url_for('selected_cocktail', selected_cocktail=cocktail))
