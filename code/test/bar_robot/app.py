d338c0906aa07bd9c27f3aa35a0f5c7212028dd2
Rene <Rene.Mlodoch@gmail.com>
2025-02-05 09:51:09 +0100

    Update app.py

--- code/test/bar_robot/app.py
+++ code/test/bar_robot/app.py
@@ -19,7 +19,7 @@ scale = Scale()
 
 # Ensure stepper motor is initialized
 if args.quick:
-    stepper.load_max_pos()  # Load max_pos from positions.json
+    stepper.load_positions()  # Load max_pos from positions.json
     stepper.move_to_position(stepper.nullPos + 20)
     stepper.aktuellePos = 0
     stepper.initialized = True
