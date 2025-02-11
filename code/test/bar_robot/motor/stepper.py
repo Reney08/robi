d87a2d46954c5e5e912e9546490ccd824ded7aeb
Rene <Rene.Mlodoch@gmail.com>
2025-02-11 16:17:14 +0100

    Update stepper.py

--- code/test/bar_robot/motor/stepper.py
+++ code/test/bar_robot/motor/stepper.py
@@ -8,7 +8,7 @@ class StepperMotor:
     def __init__(self):
         # Initialize the StepperMotor with logger, GPIO configuration, and file handlers
         self.logger = setup_logger()
-        self.settingsFileHandler = FileHandler('./bar_robot/settings.json')
+        self.settingsFileHandler = FileHandler()
         self.settings = self.settingsFileHandler.readJson()
         
         self.STEP = self.settings.get('STEP')
