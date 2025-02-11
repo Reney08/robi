952d43df092ea63270633833dae680eca3533f9d
Rene <Rene.Mlodoch@gmail.com>
2025-02-11 16:12:12 +0100

    Update scale.py

--- code/test/bar_robot/motor/scale.py
+++ code/test/bar_robot/motor/scale.py
@@ -6,7 +6,7 @@ from .scale_hx711 import HX711
 class Scale:
     def __init__(self):
         # Initialize the Scale with settings from the settings file
-        self.settingsFileHandler = FileHandler('/bar_robot/settings.txt')
+        self.settingsFileHandler = FileHandler('./bar_robot/settings.json')
         self.settings = self.settingsFileHandler.readSettings()
         
         self.clock_pin = self.settings.get('clock_pin')
