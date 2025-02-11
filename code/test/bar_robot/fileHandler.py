07a0aa049e1ebe627da2ba583b345b7af5f7c5d2
Rene <Rene.Mlodoch@gmail.com>
2025-02-11 16:11:02 +0100

    Update fileHandler.py

--- code/test/bar_robot/fileHandler.py
+++ code/test/bar_robot/fileHandler.py
@@ -12,62 +12,12 @@ class FileHandler:
             with open(self.filepath, 'r') as f:
                 return json.load(f)
         except FileNotFoundError:
-            # If the file is not found, print a warning and return an empty dictionary
+            # If the file is not found, print a warning and create default settings
             print(f"Warning: {self.filepath} not found. Creating default file.")
-            return {}
+            self.create_default_settings()
+            return self.readJson()
 
     def writeJson(self, data):
         # Write the given data to the file in JSON format with indentation for readability
         with open(self.filepath, 'w') as f:
             json.dump(data, f, indent=4)
-
-    def readSettings(self):
-        settings = {}
-        try:
-            # Attempt to open the settings file and read its contents
-            with open(self.filepath, 'r') as f:
-                for line in f:
-                    key, value = line.strip().split('=')
-                    settings[key] = self._convert_value(value)
-        except FileNotFoundError:
-            # If the file is not found, create it with default settings
-            print(f"Warning: {self.filepath} not found. Creating default settings.")
-            self.create_default_settings()
-            settings = self.readSettings()
-        return settings
-
-    def _convert_value(self, value):
-        # Convert the value to the appropriate type (int, float, or str)
-        try:
-            return int(value)
-        except ValueError:
-            try:
-                return float(value)
-            except ValueError:
-                return value
-
-    def create_default_settings(self):
-        # Ensure the directory exists
-        os.makedirs(os.path.dirname(self.filepath), exist_ok=True)
-        
-        default_settings = """
-            STEP=17
-            DIR=27
-            EN=23
-            schalterLinksPin=16
-            schalterRechtsPin=24
-            us_delay=950
-            uS=0.000001
-            pulse_min=150
-            pulse_max=600
-            GAIN=0
-            OFFSET=0
-            SCALE=1
-            time_constant=0.25
-            filtered=0
-            clock_pin=6
-            data_pin=5
-            calibration_factor=-0.0009405187713438978
-            """
-        with open(self.filepath, 'w') as f:
-            f.write(default_settings)
