ee68ae2e12d0a5f9a8e5b05002fa32d22b69b9c4
Rene <Rene.Mlodoch@gmail.com>
2025-02-11 15:34:36 +0100

    Update config.py

--- code/test/bar_robot/config.py
+++ code/test/bar_robot/config.py
@@ -1,5 +1,9 @@
 import os
 
 class Config:
+    # Set the secret key to a value from the environment variable 'SECRET_KEY'
+    # or use a default value if the environment variable is not set
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+    # Enable or disable debug mode
     DEBUG = True
