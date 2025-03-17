4f1e32b82890bd1de9ff70c024a7ed51f8394fcd
Rene <Rene.Mlodoch@gmail.com>
2025-03-17 09:48:35 +0100

    Update servo.py

--- code/test/bar_robot/motor/servo.py
+++ code/test/bar_robot/motor/servo.py
@@ -1,18 +1,23 @@
-from Adafruit_PCA9685 import PCA9685
-import time
-from fileHandler import FileHandler
-from logger import setup_logger
+# ServoMotor Inheriting from PCADevice
+class ServoMotor(PCADevice):
+    def __init__(self, address, channel):
+        """
+        Servo motor controlled via PCA9685.
+        :param address: I2C address of PCA board
+        :param channel: Channel number (0-15)
+        """
+        super().__init__(address, channel)
 
-class ServoMotor:
-    def __init__(self):
-        # Initialize the ServoMotor with logger, PWM, and positions from JSON file
+        # Initialize logging and settings
         self.logger = setup_logger()
         self.settingsFileHandler = FileHandler('./json/settings.json')
         self.settings = self.settingsFileHandler.readJson()
-        
-        self.pulse_min = self.settings.get('pulse_min')
-        self.pulse_max = self.settings.get('pulse_max')
 
+        # Load movement limits from settings file
+        self.pulse_min = self.settings.get('pulse_min', 150)
+        self.pulse_max = self.settings.get('pulse_max', 600)
+
+        # Compute positions
         self.mid_pos = (self.pulse_max + self.pulse_min) // 2
         self.range = self.pulse_max - self.pulse_min
         self.inactive_pos = self.mid_pos + (self.range // 9) + 20
@@ -20,42 +25,37 @@ class ServoMotor:
         self.waiting_pos = self.mid_pos + (self.range // 9) - 90
 
         self.current_position = 'inactive'
-        
-        self.pwm = PCA9685(busnum=1)
-        self.pwm.set_pwm_freq(60)
         self.positionsFileHandler = FileHandler('./json/positions.json')
         self.positions = self.positionsFileHandler.readJson()
 
-    def move_to_active(self, channel):
-        # Move the specified servo to the active position
-        self.logger.info(f"Moving servo {channel} to active position")
-        self.pwm.set_pwm(channel, 0, self.active_pos)
+    def activate(self):
+        """Move servo to the active position."""
+        self.logger.info(f"Moving servo on address {hex(self.pca.address)} channel {self.channel} to active position")
+        self.pca.set_pwm(self.channel, 0, self.active_pos)
         time.sleep(1)
         self.current_position = 'active'
 
-    def move_to_inactive(self, channel):
-        # Move the specified servo to the inactive position
-        self.logger.info(f"Moving servo {channel} to inactive position")
-        self.pwm.set_pwm(channel, 0, self.inactive_pos)
+    def deactivate(self):
+        """Move servo to the inactive position."""
+        self.logger.info(f"Moving servo on address {hex(self.pca.address)} channel {self.channel} to inactive position")
+        self.pca.set_pwm(self.channel, 0, self.inactive_pos)
         time.sleep(1)
         self.current_position = 'inactive'
 
-    def move_to_waiting(self, channel):
-        # Move the specified servo to the waiting position
-        self.logger.info(f"Moving servo {channel} to waiting position")
-        self.pwm.set_pwm(channel, 0, self.waiting_pos)
+    def move_to_waiting(self):
+        """Move the servo to a waiting position."""
+        self.logger.info(f"Moving servo on address {hex(self.pca.address)} channel {self.channel} to waiting position")
+        self.pca.set_pwm(self.channel, 0, self.waiting_pos)
         time.sleep(1)
         self.current_position = 'waiting'
-        
-    def get_status(self):
-        # Return the current status of the servo motor
-        return {
-            'current_position': self.current_position
-        }
 
-    def shutdown(self, channel):
-        # Shutdown the specified servo motor and turn off the PWM signal
-        self.logger.info(f"Shutting down servo {channel}")
-        self.move_to_inactive(channel)  # Ensure the servo is in the inactive position
-        self.pwm.set_pwm(channel, 0, 0)  # Turn off the PWM signal
-        self.logger.info(f"Servo {channel} shutdown complete")
+    def get_status(self):
+        """Return the current position of the servo."""
+        return {'current_position': self.current_position}
+
+    def shutdown(self):
+        """Shutdown the servo by moving it to inactive and turning off the PWM signal."""
+        self.logger.info(f"Shutting down servo on address {hex(self.pca.address)} channel {self.channel}")
+        self.deactivate()  # Ensure the servo is in the inactive position
+        self.pca.set_pwm(self.channel, 0, 0)  # Turn off the PWM signal
+        self.logger.info(f"Servo on address {hex(self.pca.address)} channel {self.channel} shutdown complete")
