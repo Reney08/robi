7c3d12f4b6d3520541ca8a5128f4f0b158266a51
Rene <Rene.Mlodoch@gmail.com>
2025-03-17 10:16:33 +0100

    Update stepper.py

--- code/test/bar_robot/motor/stepper.py
+++ code/test/bar_robot/motor/stepper.py
@@ -34,7 +34,7 @@ class StepperMotor:
         self.nullPos = 0
         self.standartPos = 20  # Default value
         self.initialized = False  # Ensure this attribute is set before calling init
-        self.servo = ServoMotor(address=0x41, channel=1)
+        self.servo = ServoMotor(address=0x41, channel=0)
         self.load_positions()
         # self.init()
 
